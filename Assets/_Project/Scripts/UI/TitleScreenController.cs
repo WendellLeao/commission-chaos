ba9e882ec9fb9401c1a9507e4c32426555715c84
Wendell Leão <leaowendell@outlook.com>
2026-07-14 21:44:15 -0300

    feat: add title screen with Play and Quit buttons via UI Toolkit

---
+++ Assets/_Project/Scripts/UI/TitleScreenController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+namespace _Project
+{
+    internal sealed class TitleScreenController : MonoBehaviour
+    {
+        [SerializeField] private UIDocument _uiDocument;
+        [SerializeField] private string _gameplaySceneName = "SampleScene";
+
+        private Button _playButton;
+        private Button _quitButton;
+
+        private void OnEnable()
+        {
+            VisualElement root = _uiDocument.rootVisualElement;
+
+            _playButton = root.Q<Button>("play-button");
+            _quitButton = root.Q<Button>("quit-button");
+
+            _playButton.clicked += OnPlayClicked;
+            _quitButton.clicked += OnQuitClicked;
+        }
+
+        private void OnDisable()
+        {
+            _playButton.clicked -= OnPlayClicked;
+            _quitButton.clicked -= OnQuitClicked;
+        }
+
+        private void OnPlayClicked()
+        {
+            SceneManager.LoadScene(_gameplaySceneName);
+        }
+
+        private void OnQuitClicked()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
